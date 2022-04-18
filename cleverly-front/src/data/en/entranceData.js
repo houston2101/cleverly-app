f19c58d3220d5a4555d0f1c0b11ccbdf7eeae257
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-04-18 16:15:54 +0300

    18.04. little changes

---
+++ cleverly-front/src/data/en/entranceData.js
@@ -0,0 +1,10 @@
+const entranceData = {
+	mainTitle: [{placeholder: 'Log in'}, {placeholder: 'Sign up'}],
+
+	name: 'Name',
+	email: 'E-mail',
+	password: 'Password',
+	repeatPassword: 'Repeat password'
+}
+
+export default entranceData
