ef5251379e26baebb0107eabda52fe034c369376
fadeeva-codedot <fadeeva.codedot@gmail.com>
2022-04-22 17:38:48 +0300

    22.04. user settings component (base template)

---
+++ cleverly-front/src/data/en/userPageData.js
@@ -0,0 +1,3 @@
+const userPageData = {}
+
+export default userPageData
--- cleverly-front/src/pages/userPage.js
+++ cleverly-front/src/pages/userPage.js
@@ -1,3 +1,31 @@
-const UserPage = () => <h1>User page</h1>
+import userPageData from '../data/en/userPageData'
+
+const UserPage = () => (
+	<div class='userSettingsWrapper'>
+		<div class='userInfoWrapper'>
+			<div class='avatarWrapper'>
+				<img />
+			</div>
+			<div class='userDataWrapper'>
+				<div class='userNameWrapper'>
+					<span class='userName'></span>
+					<div class='editUserNameIcon'></div>
+				</div>
+				<span class='email'></span>
+			</div>
+		</div>
+
+		<div class='passwordSettingsWrapper'>
+			<input class='oldPassword'></input>
+			<input class='newPassword'></input>
+			<input class='repeatPassword'></input>
+			<button class='changePasswordButton'></button>
+		</div>
+
+		<div class='themeToggle'>
+			<img />
+		</div>
+	</div>
+)
 
 export default UserPage
