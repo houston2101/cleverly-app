a0528bf5015c4ebe8313768f3a99a59e71e53b15
MitsuruYano <ghostsword2409@gmail.com>
2022-05-26 22:40:44 +0300

    Text

--- cleverly-back/routes/auth.routes.js
+++ cleverly-back/routes/auth.routes.js
@@ -44,7 +44,9 @@ router.post(
 			})
 			await user.save()
 
-			res.status(201).json({message: 'User created successfully'})
+			res.status(201).json({
+				message: 'User commit text created successfully'
+			})
 		} catch (err) {
 			res.status(500).json({message: 'Error! Try again later'})
 		}
